28022412932c0b57f801dac2ea8ca7514ed4f911
Liran Cohen <iliran11@gmail.com>
2024-09-14 17:27:36 +0300

    refactor flow

--- index.js
+++ index.js
@@ -1,37 +1,42 @@
 require("dotenv").config();
 const fns = require("./functions");
 
-const suggestionMessages = [fns.createMessage("user", fns.instruction.getSuggestion())];
-const messages = [];
-
-async function chatWithGPT() {
-  if (suggestionMessages.length < 2) {
-    const { answer } = await fns.callOpenAi({
-      messages: suggestionMessages,
-      json: true,
-    });
-    suggestionMessages.push(fns.createMessage("assistant", answer.topic));
-    if (!answer.topic) {
-      throw new Error("Something is wrong with the subject provided by OpenAI");
-    }
-    console.log(`[Bot][Suggestion]: ${answer.topic}`);
-    messages.push(
-      fns.createMessage("system", fns.instruction.getStoryTelling(answer.topic))
-    );
-    fns.rendermarkdown(`# ${answer.topic}`);
-    chatWithGPT();
-  } else {
-    const { answer } = await fns.callOpenAi({ messages });
-    if (answer.chapterProgress === 4) {
-      process.exit();
-    }
-    console.log(`[Bot][Answer] - Progess - ${answer.chapterProgress}`);
-    fns.rendermarkdown(`## ${answer.chapterTitle}`);
-    fns.rendermarkdown(answer.chapterContent);
-    messages.push(fns.createMessage("assistant", answer));
-    messages.push(fns.createMessage("user", "continue"));
-    chatWithGPT();
+const getTopic = async () => {
+  const suggestionMessages = [
+    fns.createMessage("user", fns.instruction.getSuggestion()),
+  ];
+  const { answer } = await fns.callOpenAi({
+    messages: suggestionMessages,
+    json: true,
+  });
+  suggestionMessages.push(fns.createMessage("assistant", answer.topic));
+  if (!answer.topic) {
+    throw new Error("Something is wrong with the subject provided by OpenAI");
   }
+  console.log(`[Bot][Suggestion]: ${answer.topic}`);
+  return answer.topic;
+};
+
+async function chatWithGPT(messages) {
+  const { answer } = await fns.callOpenAi({ messages });
+  if (answer.chapterProgress === 4) {
+    process.exit();
+  }
+  console.log(`[Bot][Answer] - Progess - ${answer.chapterProgress}`);
+  fns.rendermarkdown(`## ${answer.chapterTitle}`);
+  fns.rendermarkdown(answer.chapterContent);
+  messages.push(fns.createMessage("assistant", answer));
+  messages.push(fns.createMessage("user", "continue"));
+  chatWithGPT(messages);
 }
 
-chatWithGPT();
+const run = async () => {
+  const messages = [];
+  const topic = await getTopic();
+  messages.push(
+    fns.createMessage("system", fns.instruction.getStoryTelling(topic))
+  );
+  await chatWithGPT(messages);
+};
+
+run();
