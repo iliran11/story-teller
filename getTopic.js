6406b457aa3e387814a386705d47af588a701b1e
Liran Cohen <iliran11@gmail.com>
2024-09-21 15:28:29 +0300

    Maintain used topics

--- getTopic.js
+++ getTopic.js
@@ -1,6 +1,12 @@
 const fns = require("./functions");
+const fs = require("fs");
 
 const getTopic = async () => {
+  const usedTopics = fs
+    .readFileSync("./instructions/used-topics.txt", "utf-8")
+    .replace("\n", " ,");
+  let topicSuggestion = fns.instruction.getSuggestion();
+  topicSuggestion = `${topicSuggestion} Do not used the following topics: ${usedTopics}`;
   const suggestionMessages = [
     fns.createMessage("user", fns.instruction.getSuggestion()),
   ];
@@ -12,6 +18,7 @@ const getTopic = async () => {
   if (!answer.topic) {
     throw new Error("Something is wrong with the subject provided by OpenAI");
   }
+  fs.appendFileSync("./instructions/used-topics.txt", "utf-8");
   console.log(`[Bot][Suggestion]: ${answer.topic}`);
   return answer.topic;
 };
